0a60486264b1aea144dfa57e4f412b474fca1009
Shubham Chodankar <106829852+11Lost@users.noreply.github.com>
2026-06-20 19:19:09 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+
+export default {
+  register() {},
+
+  bootstrap({ strapi }) {
+    strapi.server.use(async (ctx, next) => {
+      if (
+        ctx.path === '/' ||
+        (
+          !ctx.path.startsWith('/api') &&
+          !ctx.path.startsWith('/admin') &&
+          !ctx.path.startsWith('/uploads') &&
+          !ctx.path.includes('.')
+        )
+      ) {
+        const indexPath = path.join(
+          process.cwd(),
+          'public',
+          'index.html'
+        );
+
+        if (fs.existsSync(indexPath)) {
+          ctx.type = 'html';
+          ctx.body = fs.createReadStream(indexPath);
+          return;
+        }
+      }
+
+      await next();
+    });
+  },
+};
